1a0e5a972212047f4fb90380821d1f7c88245edc
darshan-gajipara <darshangajipara222@gmail.com>
2025-01-23 15:21:04 +0530

    version update

--- index.js
+++ index.js
@@ -11,7 +11,5 @@ function calculator(num1,num2,opration) {
     if(opration === "/"){
         return (num1/num2)
     }
-}
-// console.log(calculator(55,5,"/"))
-  
+} 
   module.exports = calculator
